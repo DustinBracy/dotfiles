ea824b0a0c63e918b19610812451a17fa958c851
Dustin Bracy <dustin@bracytechnology.com>
2026-02-27 21:55:53 +0000

    add install script for initialization

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+yay -S hyprland \
+    hyprlock \
+    hyprpaper \
+    hyprlauncher \
+    waybar \
+    swaync \
+    pipewire \
+    wireplumber \
+    xdg-desktop-portal-hyprland \
+    xdg-desktop-portal-gtk \
+    hyprpolkitagent \
+    hyprshot \
+    qt5-wayland \
+    qt6-wayland \
+    kitty \
+    pipewire-pulse \
+    pavucontrol
